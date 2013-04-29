91a2046c3a045d2957d30b5aa5c604e5efb88f7e
brajac709 <bij4@cornell.edu>
2013-04-29 15:51:15 -0400

    added foo -- squre

---
+++ src/foo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int foo(int x) {
+    return x*x;
+}
+
+
+int main(int argc, char *argv) {
+    int i;
+    for(i=0; i < 10; i++) {
+        printf("i = %d, foo(i) = %d\n", i, foo(i));
+    }
+    return 0;
+}
